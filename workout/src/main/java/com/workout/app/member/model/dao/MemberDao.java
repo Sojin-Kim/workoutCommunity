767c111141f0c7b8d43c269bd7afe3fb9b34dc51
kshwan1379@naver.com <kshwan1379@naver.com>
2022-08-11 14:38:50 +0900

    테스트용 db 가져오기 성공!

---
+++ workout/src/main/java/com/workout/app/member/model/dao/MemberDao.java
@@ -0,0 +1,13 @@
+package com.workout.app.member.model.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.workout.app.member.model.dto.Member;
+
+@Mapper
+public interface MemberDao {
+	
+	List<Member> selectMemberList(); 
+}
---
+++ workout/src/main/java/com/workout/app/member/model/service/MemberService.java
@@ -0,0 +1,11 @@
+package com.workout.app.member.model.service;
+
+import java.util.List;
+
+import com.workout.app.member.model.dto.Member;
+
+public interface MemberService {
+
+	List<Member> selectMemberList();
+
+}
